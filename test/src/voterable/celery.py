f288ad9d05691e56de2a9b494a3b0477ece739fa
Sachin <sachin_rcz@yahoo.co.in>
2018-12-22 18:58:08 +0530

    celery run test

--- test/src/voterable/celery.py
+++ test/src/voterable/celery.py
@@ -7,6 +7,7 @@ from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voterable.settings")
 
+# app = Celery('voterable')
 app = Celery('voterable', broker=settings.CELERY_BROKER_URL)
 
 # Using a string here means the worker don't have to serialize
@@ -18,14 +19,18 @@ app.config_from_object('django.conf:settings')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks(settings.INSTALLED_APPS)
 
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
 app.conf.beat_schedule = {
 
-    # 'add-every-10-seconds': {
-    #     'task': 'send-email-task',
-    #     'schedule': 10.0,
-    # },
+    'add-every-10-seconds': {
+        'task': 'send-email-task',
+        'schedule': 300.0,
+    },
 
-    # # send email every monday at 8 a.m.
+    # send email every monday at 8 a.m.
     # 'send_email_monday': {
     #     'task': 'send-email-task',
     #     'schedule': crontab(hour=8, minute=0, day_of_week=1),
