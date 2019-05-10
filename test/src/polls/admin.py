e9a68e485e2f0209a714a7dc81d5a77657815d2a
yu <yuong1979@gmail.com>
2019-05-10 14:33:40 +0800

    fixed poll admin so pytest runs w/o errors

--- test/src/polls/admin.py
+++ test/src/polls/admin.py
@@ -1,62 +1,62 @@
 from django.contrib import admin
-# from django import forms
-# from polls.models import PollItem, PollFav, Ptype, SuggestedPoll, PollVoting
+from django import forms
+from polls.models import PollItem, PollFav, Ptype, SuggestedPoll, PollVoting
 
 
 
 
 
 
-# class PollModelForm(forms.ModelForm):
-#     description = forms.CharField(widget=forms.Textarea)
+class PollModelForm(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea)
 
-#     class Meta:
-#         fields = ('title', 'user_submit', 'description', 'score')
-#         model = PollItem
-
-
-# class PtypeAdmin(admin.ModelAdmin):
-#     list_display = ('id','title', 'active', 'freepoll','locked', 'c_user', 'vote_count', 'date')
-#     search_fields = ['title']
-#     list_filter = ('active', 'freepoll','locked')
-
-#     def __str__(self,obj):
-#         return obj.__str__()
-
-# class PollAdmin(admin.ModelAdmin):
-#     list_display = ('id','title','polltype', 'allowed', 'published', 'user_submit', 'score', 'pollmodifydate')
-#     search_fields = ['title']
-#     forms = PollModelForm
-
-#     def __str__(self,obj):
-#         return obj.__str__()
-
-# class PollVotingAdmin(admin.ModelAdmin):
-#     list_display = ('id','vote_user','poll','vote')
-#     list_filter = ['vote_user']
+    class Meta:
+        fields = ('title', 'user_submit', 'description', 'score')
+        model = PollItem
 
 
+class PtypeAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'active', 'freepoll','locked', 'c_user', 'vote_count', 'date')
+    search_fields = ['title']
+    list_filter = ('active', 'freepoll','locked')
 
-# class PollFavAdmin(admin.ModelAdmin):
-#     list_display = ('id','fav_user')
+    def __str__(self,obj):
+        return obj.__str__()
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('id','title','polltype', 'allowed', 'published', 'user_submit', 'score', 'pollmodifydate')
+    search_fields = ['title']
+    forms = PollModelForm
+
+    def __str__(self,obj):
+        return obj.__str__()
+
+class PollVotingAdmin(admin.ModelAdmin):
+    list_display = ('id','vote_user','poll','vote')
+    list_filter = ['vote_user']
 
 
-# class SuggestedPollAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'typePoll', 'title', 'user_submit', 'allowed', 'date', 'score')
-#     list_filter = ('typePoll', 'allowed')
+
+class PollFavAdmin(admin.ModelAdmin):
+    list_display = ('id','fav_user')
 
 
-# # class SuggestVoteAdmin(admin.ModelAdmin):
-# #     list_display = ('id','spoll','updated','date')
-#     # list_display = ('id','vote_user','spoll','updated','date')
+class SuggestedPollAdmin(admin.ModelAdmin):
+    list_display = ('id', 'typePoll', 'title', 'user_submit', 'allowed', 'date', 'score')
+    list_filter = ('typePoll', 'allowed')
 
 
-# admin.site.register(Ptype, PtypeAdmin)
-# admin.site.register(PollItem, PollAdmin)
-# admin.site.register(PollVoting, PollVotingAdmin)
-# admin.site.register(PollFav, PollFavAdmin)
+# class SuggestVoteAdmin(admin.ModelAdmin):
+#     list_display = ('id','spoll','updated','date')
+    # list_display = ('id','vote_user','spoll','updated','date')
 
-# admin.site.register(SuggestedPoll, SuggestedPollAdmin)
-# # admin.site.register(SuggestVote, SuggestVoteAdmin)
+
+admin.site.register(Ptype, PtypeAdmin)
+admin.site.register(PollItem, PollAdmin)
+admin.site.register(PollVoting, PollVotingAdmin)
+admin.site.register(PollFav, PollFavAdmin)
+
+admin.site.register(SuggestedPoll, SuggestedPollAdmin)
+# admin.site.register(SuggestVote, SuggestVoteAdmin)
 
 
